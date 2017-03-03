ab6377c681423f3b7f3ea79a3455920476c5f861
qiang.shen <qiang..shen@ele.me>
2017-03-03 20:18:14 +0800

    fix for ci

--- FlexBoxLayout/Classes/Component/FBSentinel.m
+++ FlexBoxLayout/Classes/Component/FBSentinel.m
@@ -7,7 +7,20 @@
 //
 
 #import "FBSentinel.h"
+#import <libkern/OSAtomic.h>
 
-@implementation FBSentinel
+@implementation FBSentinel {
+  int32_t _value;
+}
+
+- (int32_t)value
+{
+  return _value;
+}
+
+- (int32_t)increase {
+  return OSAtomicIncrement32(&_value);
+}
+
 
 @end
